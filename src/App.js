0cccc9644b6827baf0708bb759ca6fe2fc1d29fa
Harita Ravindranath <haritagr19@gmail.com>
2023-06-21 03:24:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./components/Photo";
+
+const clientID = `?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`;
+const mainURL = "https://api.unsplash.com/photos/";
+const searchURL = "https://api.unsplash.com/search/photos/";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchURL}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainURL}${clientID}${urlPage}`;
+    }
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos((oldPhoto) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhoto, ...data.results];
+        } else {
+          return [...oldPhoto, ...data];
+        }
+      });
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        (!loading && window.innerHeight + window.scrollY) >=
+        document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+    return () => window.removeEventListener("scroll", event);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPage(1);
+    fetchImages();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input
+            type="text"
+            placeholder="search"
+            className="form-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos?.map((image, index) => {
+            return <Photo key={index} {...image} />;
+          })}
+        </div>
+      </section>
+    </main>
   );
 }
 
